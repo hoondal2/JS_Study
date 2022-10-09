bd98b030ee4962831e9f800969ad050731aa16ce
hoondal2 <ghdtjsrud20@gmail.com>
2022-10-09 15:59:14 +0900

    데이터 타입 확인

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+import getType from './getType'
+
+console.log(typeof 'hello world!')
+console.log(typeof 123)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof {})
+console.log(typeof [])
+
+console.log(getType(123))
+console.log(getType(false))
+console.log(getType(null))
+console.log(getType({}))
+console.log(getType([]))
